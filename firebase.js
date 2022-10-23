1cb33fb3709d567f67b49331c8d98331a32af3a0
Eli_Mandujano <elite3210@gmail.com>
2022-10-23 13:01:55 -0500

    actualiza a la version anterior

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.0/firebase-app.js";
-  import {getFirestore,collection,addDoc,getDocs,onSnapshot} from "https://www.gstatic.com/firebasejs/9.12.0/firebase-firestore.js"
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+  import {getFirestore,collection,addDoc,getDocs,onSnapshot} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js"
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
