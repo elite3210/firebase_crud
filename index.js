1350fbd8a525d5cbb2289da736b96ff51efba91e
Eli Mandujano <elite3210@gmail.com>
2022-10-30 01:06:08 -0500

    se mueve la base datos firesbase a recursos humanos y se agrega imagenes

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 import {guardarTask,traerTasks} from './firebase.js'
 
 const tareaForm = document.getElementById('tarea-form')
-
 const tareasContainer = document.getElementById('tareas-container')
+const total_container = document.getElementById('total_container')
 
 window.addEventListener('DOMContentLoaded',async ()=>{
     console.log(traerTasks())                 
     const tasks = await traerTasks()
 
     let html = ""
+    let horas =0;
+    const dia=['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado']
+    let lista = 'Hola'
 
     tasks.forEach(doc => {
         
         const tasks = doc.data()
-        suma =64
-        html += `<tr><td>${tasks.description}</td><td>${tasks.title}</td><td>${tasks.salida}</td>
+        html += `<tr><td>${tasks.description}</td><td>${dia[`${new Date(`${tasks.title}`).getDay()}`]}</td><td>${tasks.title}</td><td>${tasks.salida}</td>
                 <td>${(((new Date(`${tasks.salida}`).getTime())-(new Date(`${tasks.title}`).getTime()))-((new Date(`${tasks.salida}`).getTime())-(new Date(`${tasks.title}`).getTime()))%(1000*60*60))/(1000*60*60)}
-                <span>:${((((new Date(`${tasks.salida}`).getTime()))-(new Date(`${tasks.title}`).getTime()))%(1000*60*60))/60000}</span>
-                </td><td>${suma}</td></tr>`
+                <span>:${((((new Date(`${tasks.salida}`).getTime()))-(new Date(`${tasks.title}`).getTime()))%(1000*60*60))/60000}</span></td>
+                <td><span>S/</span>${Math.round(((new Date(`${tasks.salida}`).getTime())-(new Date(`${tasks.title}`).getTime()))/(1000*60*60)*3.125)}</td>
+                
+                </tr>`
+        
+        horas += ((new Date(`${tasks.salida}`).getTime())-(new Date(`${tasks.title}`).getTime()))/(1000*60*60)
     });
+
+    console.log('Horas:',lista.substring(lista.length,2))
+    console.log('Importe:',horas*3.125)
     
     tareasContainer.innerHTML = html
 })
+
 
 
 tareaForm.addEventListener('submit',(e)=>{
@@ -34,7 +44,7 @@ tareaForm.addEventListener('submit',(e)=>{
     guardarTask(titulo.value,descripcion.value,salida.value)
 
     tareaForm.reset()
+    tareasContainer.reset()
 })
-   
 
 
